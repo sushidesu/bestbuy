2505bdb23b4140b839748ee7de6f161d1f793986
sushidesu <hugemerge@gmail.com>
2023-02-12 19:16:50 +0900

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,64 @@
 import "./globals.css"
 
+import { Inter } from "@next/font/google"
+import Link from "next/link"
+import { FC } from "react"
+
+import styles from "./layout.module.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={styles["html"]}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={styles["body"]}>{children}</body>
+      <footer>
+        <Footer />
+      </footer>
     </html>
+  )
+}
+
+const COPYRIGHT = "©︎ splarate"
+const FOOTER_LINKS: { label: string; href: string }[] = [
+  {
+    label: "運営",
+    href: "/",
+  },
+  {
+    label: "利用規約",
+    href: "/",
+  },
+  {
+    label: "お問合わせ",
+    href: "/",
+  },
+]
+
+const Footer: FC = () => {
+  return (
+    <section>
+      <div>
+        <ul>
+          {FOOTER_LINKS.map((link) => (
+            <li key={link.label}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <p className={inter.className}>{COPYRIGHT}</p>
+      </div>
+    </section>
   )
 }
